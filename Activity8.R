d6338c969e5b11b320d00afeb05c9c4baf4ca43d
Alison Blaine <alblaine@users.noreply.github.com>
2021-08-08 12:18:57 -0400

    Add files via upload

--- Activity8.R
+++ Activity8.R
@@ -1,16 +1,33 @@
 ## Activity 8: Document Feature Similarity
 # Goal: Find documents similar or different from another document.
 
+install.packages("quanteda.textstats")
+library(quanteda.textstats)
+library(readtext)
+library(quanteda)
+
+###### Setup -- skip if you have already done this ###
+ted_talks <- readtext("ted_talks17.csv", text_field = "transcript", encoding = "utf8")
+ted_corp <- corpus(ted_talks)
+ted_tokens <- tokens(ted_corp, remove_punct = TRUE, remove_numbers = TRUE) %>%
+  tokens_remove(stopwords("en")) %>%
+  tokens_tolower() %>%
+  tokens_wordstem()  
+
+ted_dfm <- dfm(ted_tokens)
+
+###### End Setup
+
 # I. Document similarity
 
 # 1. Start with a reference document, ted talk #1 and compute cosine similarity with 
 # other talks in the corpus.
 
-textstat_simil(ted_dfm, ted_dfm["ted_talks17.csv.1",], method="cosine")
+sim_stats <- textstat_simil(ted_dfm, ted_dfm["ted_talks17.csv.1",], method="cosine")
 
 # 2. Turn that output into a data frame
 
-similarity_scores <- data.frame(textstat_simil(ted_dfm, ted_dfm["ted_talks17.csv.1",], method="cosine"))
+similarity_scores <- data.frame(sim_stats)
 
 # 3. Sort in descending order of cosine similarity
 similarity_scores[order(-similarity_scores$cosine),]
@@ -21,7 +38,7 @@ ted_corp$tags[34]
 
 # 5. Practice find documents most similar to ted_talks17.csv.10.
 
-textstat_simil(      )
+sim_stats10 <- textstat_simil(      )
 
 # 6. Practice. Convert similarity scores to a data frame.
 similarity_scores10 <- data.frame(     )
